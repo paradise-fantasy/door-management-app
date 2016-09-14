171131e3596b38c2dad3dc782472647147367583
khrall <raymitoro@gmail.com>
2016-09-14 22:03:11 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# door-management-app
---
+++ index.android.js
@@ -0,0 +1,62 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ * @flow
+ */
+
+import React, { Component } from 'react';
+import {
+  AppRegistry,
+  StyleSheet,
+  Text,
+  View,
+  Image,
+  TouchableWithoutFeedback
+} from 'react-native';
+
+class DoorManagementApp2 extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { active: false };
+    this._onPressIn = this._onPressIn.bind(this);
+    this._onPressOut = this._onPressOut.bind(this);
+  }
+
+  _onPressIn() {
+    fetch('http://google.com').then(res => console.log(res));
+    this.setState({ active: true });
+  }
+
+  _onPressOut() {
+    this.setState({ active: false });
+  }
+
+  render() {
+    return (
+      <View style={styles.container}>
+        <TouchableWithoutFeedback onPressIn={this._onPressIn} onPressOut={this._onPressOut}>
+          <View>
+            <Image style={[styles.button, { opacity: this.state.active ? 0 : 1 }]} source={require('./images/bryter_opp.png')} />
+            <Image style={[styles.button, { top: -256, opacity: this.state.active ? 1 : 0 }]} source={require('./images/bryter_opp.png')} />
+          </View>
+        </TouchableWithoutFeedback>
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#F5FCFF',
+  },
+  button: {
+    width: 256,
+    height: 256,
+    resizeMode: 'contain'
+  },
+});
+
+AppRegistry.registerComponent('DoorManagementApp2', () => DoorManagementApp2);
