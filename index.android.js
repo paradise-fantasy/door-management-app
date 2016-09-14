57d6e3011c881b0ba394fb110d435e970dc093f7
khrall <raymitoro@gmail.com>
2016-09-14 22:26:13 +0100

    Fixed

--- index.android.js
+++ index.android.js
@@ -23,11 +23,13 @@ class DoorManagementApp2 extends Component {
   }
 
   _onPressIn() {
+    console.log('press in');
     fetch('http://google.com').then(res => console.log(res));
     this.setState({ active: true });
   }
 
   _onPressOut() {
+    console.log('press out');
     this.setState({ active: false });
   }
 
@@ -35,9 +37,9 @@ class DoorManagementApp2 extends Component {
     return (
       <View style={styles.container}>
         <TouchableWithoutFeedback onPressIn={this._onPressIn} onPressOut={this._onPressOut}>
-          <View>
+          <View style={styles.buttonContainer}>
             <Image style={[styles.button, { opacity: this.state.active ? 0 : 1 }]} source={require('./images/bryter_opp.png')} />
-            <Image style={[styles.button, { top: -256, opacity: this.state.active ? 1 : 0 }]} source={require('./images/bryter_opp.png')} />
+            <Image style={[styles.button, { top: -256, opacity: this.state.active ? 1 : 0 }]} source={require('./images/bryter_ned.png')} />
           </View>
         </TouchableWithoutFeedback>
       </View>
@@ -51,6 +53,10 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
     alignItems: 'center',
     backgroundColor: '#F5FCFF',
+  },
+  buttonContainer: {
+    width: 256,
+    height: 256
   },
   button: {
     width: 256,
